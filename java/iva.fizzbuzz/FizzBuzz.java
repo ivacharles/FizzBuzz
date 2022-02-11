460c3944bb1b0468b03e90d86f6189956e615abf
Iva Lesperance Charles <Iva.LesperanceCharles@mail.citytech.cuny.edu>
2022-02-11 13:40:21 -0500

    Fizz and Buzz are showing

--- java/iva.fizzbuzz/FizzBuzz.java
+++ java/iva.fizzbuzz/FizzBuzz.java
@@ -1,13 +1,14 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        int numbers =15;
+        int numbers = 15;
         for (int i = 1; i <= numbers; i++) {
-            if(i%3==0){
+            if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }else {
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
                 System.out.println(i);
             }
         }
-//        System.out.println("Hello, World!");
     }
 }
